315c50f7e0df4099a724bdf4ac554714d09866d7
Brian Faust <faustbrian@users.noreply.github.com>
2016-11-23 12:48:41 +0200

    Applied fixes from StyleCI (#3)
    [ci skip] [skip ci]

--- src/Models/Board.php
+++ src/Models/Board.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Laravel Leaderboard.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Leaderboard\Models;
 
 use Illuminate\Database\Eloquent\Model;
