e61e8353348f7069101a20acda623146efba5ca9
Brian Faust <faustbrian@users.noreply.github.com>
2017-02-22 10:19:19 +0200

    Apply fixes from StyleCI (#6)
    [ci skip] [skip ci]

--- src/Traits/Boardable.php
+++ src/Traits/Boardable.php
@@ -11,6 +11,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of Laravel Leaderboard.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Leaderboard\Traits;
 
 use BrianFaust\Leaderboard\Repositories\EloquentBoardRepository;
